b6a3f1ac10bf3c7bfeca843109c4c4d0077c20b8
Kim Min-Woo <107420002+kmw2378@users.noreply.github.com>
2023-07-25 23:45:21 +0900

    7/25 (화) [level1][JAVA]
    - 가장 가까운 같은 글자

---
+++ src/java/level1/nearest_char/Solution.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        int[] answer = new int[s.length()];
+        
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            
+            answer[i] = i - map.getOrDefault(c, i + 1);
+            map.put(c, i);
+        }
+        
+        return answer;
+    }
+}
