9ab75a7ab45c53c849c5b119d1a4a30943f649a9
Kim Min-Woo <107420002+kmw2378@users.noreply.github.com>
2023-11-22 23:26:21 +0900

    11/22 (수) [level1][JAVA]
    - 다트 게임

--- src/java/level1/dart_game/Solution.java
+++ src/java/level1/dart_game/Solution.java
@@ -1,72 +1,99 @@
 import java.util.*;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 class Solution {
     public int solution(String dartResult) {
-        int answer = 0;
-        List<String> list = new ArrayList<>();
-        
-        int begin = 0;
-        for (int i = 1; i < dartResult.length(); i++) {
-            char before = dartResult.charAt(i - 1);
-            char current = dartResult.charAt(i);
-            if (!isScore(before) && isScore(current)) {
-                list.add(dartResult.substring(begin, i));
-                begin = i;
-            }
-        }
-        
-        list.add(dartResult.substring(begin));
-        
-        int before = 0;
-        for (String s : list) {
-            int score = 0;
-            int i = 0;
-            while (isScore(s.charAt(i))) {
-                score = score * 10 + (int)(s.charAt(i++) - '0');
-            }
-            
-            char bonus = s.charAt(i);
-            int currentScore = getCurrentScore(score, bonus);
-            record[i] = currentScore;
-            
-            if (i == s.length() - 1) {
-                answer += currentScore;
+        Map<Character, Integer> bonusMap = initBonusMap();
+        List<String> darts = createDarts(dartResult);
+        Stack<Integer> stack = new Stack<>();
+        for (String dart : darts) {
+            char start = dart.charAt(0);
+            if (!isNumeric(start)) {
+                applyOption(start, stack);
                 continue;
             }
             
-            char option = s.charAt(s.length() - 1);
-            if (option == '*') {
-                
-                answer += (2 * (before + currentScore));
-            } else {
-                answer -= currentScore;
+            char bonus = getBonus(dart);
+            int score = getScore(dart);
+            int totalScore = (int) Math.pow(score, bonusMap.get(bonus));
+            stack.add(totalScore);
+            
+            char end = dart.charAt(dart.length() - 1);
+            if (end == '*' || end == '#') {
+                applyOption(end, stack);
             }
         }
-        
-        return answer;
+
+        return (int) stack.stream()
+            .mapToInt(e -> e)
+            .sum();
     }
     
-    private boolean isScore(char c) {
+    private boolean isNumeric(char c) {
         return '0' <= c && c <= '9';
-    }
-    
-    private int getBonus(char c) {
-        if (c == 'S') {
-            return 1;
-        }
-        
-        if (c == 'D') {
-            return 2;
-        }
-        
-        return 3;
-    }
-    
-    private int getCurrentScore(int score, char bonus) {
-        return (int)Math.pow(score, getBonus(bonus));
     }
     
     private boolean isBonus(char c) {
         return c == 'S' || c == 'D' || c == 'T';
+    }
+    
+    private List<String> createDarts(String dartResult) {
+        List<String> darts = new ArrayList<>();
+        int start = 0;
+        for (int i = 1; i < dartResult.length() - 1; i++) {
+            if (!isNumeric(dartResult.charAt(i - 1)) && isNumeric(dartResult.charAt(i))) {
+                darts.add(dartResult.substring(start, i));
+                start = i;
+            }
+        }
+        
+        darts.add(dartResult.substring(start));
+        return darts;
+    }
+    
+    private Map<Character, Integer> initBonusMap() {
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('S', 1);
+        map.put('D', 2);
+        map.put('T', 3);
+        
+        return map;
+    }
+    
+    private void applyOption(char c, Stack<Integer> stack) {
+        int pop = stack.pop();
+        if (c == '#') {
+            stack.add(-pop);
+            return;
+        }
+        
+        if (!stack.isEmpty()) {
+            stack.add(stack.pop() * 2);
+        }
+        stack.add(pop * 2);
+    }
+    
+    private int getScore(String dart) {
+        int score = 0;
+        for (char c : dart.toCharArray()) {
+            if (!isNumeric(c)) {
+                break;
+            }
+            
+            score = score * 10 + (c - '0');
+        }
+        
+        return score;
+    }
+    
+    private char getBonus(String dart) {
+        for (char c : dart.toCharArray()) {
+            if (c == 'S' || c == 'D' || c == 'T') {
+                return c;
+            }
+        }
+        
+        throw new IllegalArgumentException();
     }
 }
